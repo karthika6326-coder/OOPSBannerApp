d206369e861cc93b4dec9f70fd64d48953fe3eb3
karthika6326-coder <karthika6326@gmail.com>
2026-03-02 14:14:20 +0530

    Level-1 programs

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,68 +1,42 @@
 import java.util.HashMap;
-
 public class OOPSBannerApp {
 
-    public static HashMap<Character, String[]> createCharacterMap() {
-        HashMap<Character, String[]> charMap = new HashMap<>();
-
-        charMap.put('O', new String[] {
-            "    ***    ",
-            " **     ** ",
-            "**       **",
-            "**       **",
-            "**       **",
-            "**       **",
-            "**       **",
-            " **     ** ", 
-            "    ***    "
-        });
-
-        charMap.put('P', new String[] {
-            "******    ",
-            "**     ** ",
-            "**      **",
-            "**     ** ",
-            "******    ", 
-            "**        ",
-            "**        ",
-            "**        ",
-            "**        ",
-        });
-
-        charMap.put('S', new String[] {
-            "     *****  ",
-            "  **        ",
-            " **         ",
-            "  **        ",
-            "    ****    ",
-            "         ** ",
-            "          **",
-            "         ** ",
-            "   *****   ",  
-        });
-
-        return charMap;
-    }
-
-    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
-        if (message == null || message.isEmpty()) return;
-
-        int patternHeight = charMap.get('O').length;
-
-        for (int line = 0; line < patternHeight; line++) {
-            StringBuilder bannerLine = new StringBuilder();
-            for (char c : message.toCharArray()) {
-                if (charMap.containsKey(c)) {
-                    bannerLine.append(charMap.get(c)[line]).append("  ");
-                }
-            }
-            System.out.println(bannerLine.toString());
-        }
-    }
-
     public static void main(String[] args) {
-        HashMap<Character, String[]> charMap = createCharacterMap();
-        String message = "OOPS";
-        displayBanner(message, charMap);
+        calculateHarryAge();
+        calculateSamAverage();
+        convertDistance();
+        calculateProfit();
+    }
+
+    public static void calculateHarryAge() {
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+
+    public static void calculateSamAverage() {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+
+    public static void convertDistance() {
+        double km = 10.8;
+        double miles = km * 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+
+    public static void calculateProfit() {
+        double costPrice = 129.0;
+        double sellingPrice = 191.0;
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+
+        System.out.printf("The Cost Price is INR %.0f and Selling Price is INR %.0f\n" +
+                          "The Profit is INR %.0f and the Profit Percentage is %.2f%%\n", 
+                          costPrice, sellingPrice, profit, profitPercentage);
     }
 }
