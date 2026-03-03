ee6072648be4d3942f6f3d1077b747260d3b939f
karthika6326-coder <karthika6326@gmail.com>
2026-03-03 15:18:53 +0530

    week 2 level 2 programs

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,48 +3,47 @@ import java.util.Scanner;
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        checkDivisibility();
-        checkSmallest();
-        checkLargest();
-        sumNaturalNumbers();
+        printOddEvenRange();
+        calculateEmployeeBonus();
+        printMultiplicationTable();
     }
 
-    public static void checkDivisibility() {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        boolean isDivisible = (number % 5 == 0);
-        System.out.println("Is the number " + number + " divisible by 5? " + isDivisible);
-    }
-
-    public static void checkSmallest() {
-        Scanner scanner = new Scanner(System.in);
-        int n1 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int n3 = scanner.nextInt();
-        boolean isSmallest = (n1 < n2 && n1 < n3);
-        System.out.println("Is the first number the smallest? " + isSmallest);
-    }
-
-    public static void checkLargest() {
-        Scanner scanner = new Scanner(System.in);
-        int n1 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int n3 = scanner.nextInt();
-
-        System.out.println("Is the first number the largest? " + (n1 > n2 && n1 > n3));
-        System.out.println("Is the second number the largest? " + (n2 > n1 && n2 > n3));
-        System.out.println("Is the third number the largest? " + (n3 > n1 && n3 > n2));
-    }
-
-    public static void sumNaturalNumbers() {
+    public static void printOddEvenRange() {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
         if (number >= 1) {
-            int sum = number * (number + 1) / 2;
-            System.out.println("The sum of " + number + " natural numbers is " + sum);
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even number");
+                } else {
+                    System.out.println(i + " is odd number");
+                }
+            }
         } else {
             System.out.println("The number " + number + " is not a natural number");
+        }
+    }
+
+    public static void calculateEmployeeBonus() {
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+        int yearsOfService = scanner.nextInt();
+        double bonus = 0;
+
+        if (yearsOfService > 5) {
+            bonus = salary * 0.05;
+        }
+
+        System.out.println("The bonus amount is " + bonus);
+    }
+
+    public static void printMultiplicationTable() {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
         }
     }
 }
