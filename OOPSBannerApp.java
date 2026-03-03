dc66143ac982061ed6a31ed5a8c8756aa212e19d
karthika6326-coder <karthika6326@gmail.com>
2026-03-03 14:59:19 +0530

    level-3 programs

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,41 +3,21 @@ import java.util.Scanner;
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        calculateBasicOperations();
-        calculateTriangleArea();
-        calculateSquareSide();
+        convertCelsiusToFahrenheit();
+        convertFahrenheitToCelsius();
     }
 
-    public static void calculateBasicOperations() {
+    public static void convertCelsiusToFahrenheit() {
         Scanner scanner = new Scanner(System.in);
-        double number1 = scanner.nextDouble();
-        double number2 = scanner.nextDouble();
-
-        double add = number1 + number2;
-        double sub = number1 - number2;
-        double mul = number1 * number2;
-        double div = number1 / number2;
-
-        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers " + number1 + " and " + number2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+        double celsius = scanner.nextDouble();
+        double farenheitResult = (celsius * 9/5) + 32;
+        System.out.println("The " + celsius + " celsius is " + farenheitResult + " fahrenheit");
     }
 
-    public static void calculateTriangleArea() {
+    public static void convertFahrenheitToCelsius() {
         Scanner scanner = new Scanner(System.in);
-        double base = scanner.nextDouble();
-        double height = scanner.nextDouble();
-
-        double areaCm = 0.5 * base * height;
-        double areaIn = areaCm / (2.54 * 2.54);
-
-        System.out.println("The Area of the triangle in sq in is " + areaIn + " and sq cm is " + areaCm);
-    }
-
-    public static void calculateSquareSide() {
-        Scanner scanner = new Scanner(System.in);
-        double perimeter = scanner.nextDouble();
-
-        double side = perimeter / 4;
-
-        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+        double fahrenheit = scanner.nextDouble();
+        double celsiusResult = (fahrenheit - 32) * 5/9;
+        System.out.println("The " + fahrenheit + " fahrenheit is " + celsiusResult + " celsius");
     }
 }
