38341afeac410bda9c73d980d8181407dcd073ed
karthika6326-coder <karthika6326@gmail.com>
2026-03-26 14:40:34 +0530

    Level 1 programs

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,41 +1,12 @@
-import java.util.Scanner;
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        checkLeapYearNested();
-        checkLeapYearLogical();
-    }
-
-    public static void checkLeapYearNested() {
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-
-        if (year >= 1582) {
-            if (year % 400 == 0) {
-                System.out.println(year + " is a Leap Year");
-            } else if (year % 100 == 0) {
-                System.out.println(year + " is not a Leap Year");
-            } else if (year % 4 == 0) {
-                System.out.println(year + " is a Leap Year");
-            } else {
-                System.out.println(year + " is not a Leap Year");
-            }
-        } else {
-            System.out.println("Year must be 1582 or later");
-        }
-    }
-
-    public static void checkLeapYearLogical() {
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-
-        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
-            System.out.println(year + " is a Leap Year");
-        } else if (year >= 1582) {
-            System.out.println(year + " is not a Leap Year");
-        } else {
-            System.out.println("Year must be 1582 or later");
-        }
+       System.out.println(args[0]);
+	   int[] numbers = new int[5];
+	   for(int i=0;i<numbers.length;i++){
+		   numbers[i]=i+1;
+	   }
+	   for(int i=0;i<numbers.length;i++){
+		System.out.println(numbers[i]);   
+	   }
     }
 }
