55303b20218a7905c7e2380489a3aba2cb1225d9
karthika6326-coder <karthika6326@gmail.com>
2026-04-19 22:41:25 +0530

    fourth usecase

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,12 @@
-public class HelloApp {
+public class HelloAppUC4 {
 
     public static void main(String[] args) {
 
-        String name = "World";
-
         if (args.length > 0) {
-            name = args[0];
+            String names = String.join(" ", args);
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
         }
-
-        System.out.println("Hello, " + name + "!");
     }
 }
