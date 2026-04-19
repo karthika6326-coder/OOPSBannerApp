597e2fdc854d9b4f2501a61fbe8eb08c2a87a7ad
karthika6326-coder <karthika6326@gmail.com>
2026-04-19 22:34:43 +0530

    third usecase

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
-
 public class HelloApp {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Hello " + name);
+
+    public static void main(String[] args) {
+
+        String name = "World";
+
+        if (args.length > 0) {
+            name = args[0];
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
