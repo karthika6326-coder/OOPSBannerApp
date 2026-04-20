27d708373a4ee415b65e3c7734c6b8f3602c6493
karthika6326-coder <karthika6326@gmail.com>
2026-04-20 13:34:05 +0530

    third usecase

--- Tic-Tac-ToeApp.java
+++ Tic-Tac-ToeApp.java
@@ -1,37 +1,18 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    static boolean isHumanTurn;
-    static char humanSymbol;
-    static char computerSymbol;
-
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
 
-    static void tossAndAssignSymbols() {
-        Random random = new Random();
-        if (random.nextBoolean()) {
-            isHumanTurn = true;
-            humanSymbol = 'X';
-            computerSymbol = 'O';
-        } else {
-            isHumanTurn = false;
-            humanSymbol = 'O';
-            computerSymbol = 'X';
-        }
-    }
-
-    static void displayTossResult() {
-        if (isHumanTurn) {
-            System.out.println("Human plays first with symbol " + humanSymbol);
-            System.out.println("Computer will use symbol " + computerSymbol);
-        } else {
-            System.out.println("Computer plays first with symbol " + computerSymbol);
-            System.out.println("Human will use symbol " + humanSymbol);
-        }
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1–9): ");
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
+
 
