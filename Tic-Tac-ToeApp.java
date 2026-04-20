9767f529f2dd2b9654a64141cd6a50692372270d
karthika6326-coder <karthika6326@gmail.com>
2026-04-20 13:45:22 +0530

    fifth usecase

--- Tic-Tac-ToeApp.java
+++ Tic-Tac-ToeApp.java
@@ -1,17 +1,22 @@
 public class TicTacToe {
 
+    static char[][] board = {
+        {'_', '_', '_'},
+        {'_', '_', '_'},
+        {'_', '_', '_'}
+    };
+
     public static void main(String[] args) {
-        int slot = 7;
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        System.out.println(isValidMove(1, 1));
+        System.out.println(isValidMove(3, 3));
+        System.out.println(isValidMove(-1, 0));
     }
 
-    static int getRowFromSlot(int slot) {
-        return (slot - 1) / 3;
-    }
-
-    static int getColFromSlot(int slot) {
-        return (slot - 1) % 3;
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            return false;
+        }
+        return board[row][col] == '_';
     }
 }
 
