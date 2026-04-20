734d89ebf42e3cbcc70954325fe41065e5b07693
karthika6326-coder <karthika6326@gmail.com>
2026-04-20 13:29:33 +0530

    second usecase

--- Tic-Tac-ToeApp.java
+++ Tic-Tac-ToeApp.java
@@ -1,29 +1,37 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
     }
 
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human plays first with symbol " + humanSymbol);
+            System.out.println("Computer will use symbol " + computerSymbol);
+        } else {
+            System.out.println("Computer plays first with symbol " + computerSymbol);
+            System.out.println("Human will use symbol " + humanSymbol);
         }
     }
 }
+
