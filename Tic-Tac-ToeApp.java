767528df16749b93a53c0a52cd62d0c398116516
karthika6326-coder <karthika6326@gmail.com>
2026-04-21 15:04:00 +0530

    seventh usecase

--- Tic-Tac-ToeApp.java
+++ Tic-Tac-ToeApp.java
@@ -1,14 +1,30 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'_', '_', '_'},
+        {'_', '_', '_'},
+        {'_', '_', '_'}
+    };
+
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println(board[0][0]);
+        computerMove();
     }
 
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
+    static void computerMove() {
+        Random rand = new Random();
+        int row, col;
+        while (true) {
+            row = rand.nextInt(3);
+            col = rand.nextInt(3);
+            if (board[row][col] == '_') {
+                board[row][col] = computerSymbol;
+                break;
+            }
+        }
     }
 }
 
