ddf7ece57393c6134061b5ed6674587e33301139
karthika6326-coder <karthika6326@gmail.com>
2026-04-21 15:00:17 +0530

    sixth usecase

--- Tic-Tac-ToeApp.java
+++ Tic-Tac-ToeApp.java
@@ -1,57 +1,17 @@
-import java.util.Scanner;
+public class TicTacToe {
 
-public class BMICalculator {
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        double[][] data = new double[10][2];
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Enter weight (kg) for person " + (i + 1) + ": ");
-            data[i][0] = scanner.nextDouble();
-            System.out.print("Enter height (cm) for person " + (i + 1) + ": ");
-            data[i][1] = scanner.nextDouble();
-        }
-
-        String[][] results = computeBMIResults(data);
-        displayResults(results);
-
-        scanner.close();
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
     }
 
-    static String[][] computeBMIResults(double[][] data) {
-        String[][] results = new String[10][4];
-        for (int i = 0; i < 10; i++) {
-            double weight = data[i][0];
-            double heightCm = data[i][1];
-            double heightM = heightCm / 100.0;
-            double bmi = weight / (heightM * heightM);
-            String status = getBMIStatus(bmi);
-
-            results[i][0] = String.format("%.1f", heightCm);
-            results[i][1] = String.format("%.1f", weight);
-            results[i][2] = String.format("%.1f", bmi);
-            results[i][3] = status;
-        }
-        return results;
-    }
-
-    static String getBMIStatus(double bmi) {
-        if (bmi <= 18.4) return "Underweight";
-        else if (bmi <= 24.9) return "Normal";
-        else if (bmi <= 39.9) return "Overweight";
-        else return "Obese";
-    }
-
-    static void displayResults(String[][] results) {
-        System.out.printf("%-10s %-10s %-10s %-15s%n", "Height(cm)", "Weight(kg)", "BMI", "Status");
-        System.out.println("----------------------------------------------------------");
-        for (int i = 0; i < results.length; i++) {
-            System.out.printf("%-10s %-10s %-10s %-15s%n",
-                    results[i][0], results[i][1], results[i][2], results[i][3]);
-        }
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
+
 
 
 
